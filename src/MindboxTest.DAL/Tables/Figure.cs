e5bc382d2b76a4061fe52c7b2d94e9b166bcae69
Renat <renatmusaev@yandex.ru>
2020-12-05 13:43:18 +0400

    add tables

---
+++ src/MindboxTest.DAL/Tables/Figure.cs
@@ -0,0 +1,8 @@
+﻿namespace MindboxTest.DAL.Tables
+{
+    public sealed class Figure
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ src/MindboxTest.DAL/Tables/FigureAreaParam.cs
@@ -0,0 +1,9 @@
+﻿namespace MindboxTest.DAL.Tables
+{
+    public sealed class FigureAreaParam
+    {
+        public long FigureId { get; set; }
+        public string ParameterName { get; set; }
+        public double ParameterValue { get; set; }
+    }
+}
