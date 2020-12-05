34af355de3975c82b6539632d3b14294af06c9bc
Renat <renatmusaev@yandex.ru>
2020-12-05 13:43:36 +0400

    add dto

---
+++ src/MindboxTest.DAL/Dto/FigureWithParamsDto.cs
@@ -0,0 +1,18 @@
+﻿using MindboxTest.DAL.Tables;
+using System;
+using System.Collections.Generic;
+
+namespace MindboxTest.DAL.Dto
+{
+    public class FigureWithParamsDto
+    {
+        public FigureWithParamsDto(Figure fig, IEnumerable<FigureAreaParam> @params)
+        {
+            Figure = fig ?? throw new ArgumentNullException(nameof(fig));
+            Params = @params ?? throw new ArgumentNullException(nameof(@params));
+        }
+
+        public Figure Figure { get; }
+        public IEnumerable<FigureAreaParam> Params { get; }
+    }
+}
