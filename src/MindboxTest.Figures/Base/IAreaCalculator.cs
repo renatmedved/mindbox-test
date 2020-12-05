d928816cf590a01bcf0518d2069ffba14cd86344
Renat <renatmusaev@yandex.ru>
2020-12-05 10:57:37 +0400

    base calculator

---
+++ src/MindboxTest.Figures/Base/IAreaCalculator.cs
@@ -0,0 +1,9 @@
+﻿using MindboxTest.Contracts;
+
+namespace MindboxTest.Figures.Base
+{
+    public interface IAreaCalculator<TFigureDescription>
+    {
+        Result<double> Calculate(TFigureDescription desc);
+    }
+}
