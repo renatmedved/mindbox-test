f0f12523b364e06651cfed86d12274fb834ed951
Renat <renatmusaev@yandex.ru>
2020-12-05 13:42:55 +0400

    add connection factory

---
+++ src/MindboxTest.DAL/Connections/ConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SQLite;
+
+namespace MindboxTest.DAL.Connections
+{
+    public class ConnectionFactory
+    {
+        private readonly string _dbFile;
+
+        public ConnectionFactory(string dbFile)
+        {
+            _dbFile = dbFile;
+        }
+
+        public SQLiteConnection MakeDbConnection()
+        {
+            return new SQLiteConnection("Data Source=" + _dbFile);
+        }
+    }
+}
