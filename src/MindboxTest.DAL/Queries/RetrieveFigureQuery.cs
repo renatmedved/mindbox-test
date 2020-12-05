8d4d015208d6e144ed27fc290c385193ad95c578
Renat <renatmusaev@yandex.ru>
2020-12-05 13:44:00 +0400

    add queries

---
+++ src/MindboxTest.DAL/Queries/RetrieveFigureQuery.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+
+using MindboxTest.DAL.Connections;
+using MindboxTest.DAL.Dto;
+using MindboxTest.DAL.Tables;
+
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Threading.Tasks;
+
+namespace MindboxTest.DAL.Queries
+{
+    public class RetrieveFigureQuery
+    {
+        private readonly ConnectionFactory _connFactory;
+
+        public RetrieveFigureQuery(ConnectionFactory connFactory)
+        {
+            _connFactory = connFactory;
+        }
+
+        public async Task<FigureWithParamsDto> Execute(long id)
+        {
+            using SQLiteConnection conn = _connFactory.MakeDbConnection();
+            await conn.OpenAsync();
+
+            Figure figure = await conn.QueryFirstAsync<Figure>(
+                @"SELECT Id, Type 
+                  FROM Figure
+                  WHERE Id = @id", new { id });
+
+            if (figure == null)
+            {
+                return null;
+            }
+
+            IEnumerable<FigureAreaParam> @params = await conn.QueryAsync<FigureAreaParam>(
+                @"SELECT FigureId, ParameterName, ParameterValue
+                  FROM FigureAreaParam
+                  WHERE FigureId = @id", new { id });
+
+            return new FigureWithParamsDto(figure, @params);
+        }
+    }
+}
---
+++ src/MindboxTest.DAL/Queries/SaveFigureQuery.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+
+using MindboxTest.DAL.Connections;
+using MindboxTest.DAL.Dto;
+using MindboxTest.DAL.Tables;
+
+using System.Data.SQLite;
+using System.Threading.Tasks;
+
+namespace MindboxTest.DAL.Queries
+{
+    public class SaveFigureQuery
+    {
+        private readonly ConnectionFactory _connFactory;
+
+        public SaveFigureQuery(ConnectionFactory connFactory)
+        {
+            _connFactory = connFactory;
+        }
+
+        public async Task<long> Execute(FigureWithParamsDto dto)
+        {
+            using SQLiteConnection conn = _connFactory.MakeDbConnection();
+            await conn.OpenAsync();
+
+            long id = await conn.QuerySingleAsync<long>(
+                @"INSERT INTO Figure
+                ( Type ) VALUES
+                ( @Type );
+                select last_insert_rowid()", dto.Figure);
+
+            foreach(FigureAreaParam param in dto.Params)
+            {
+                param.FigureId = id;
+
+                await conn.QueryAsync(
+                    @"INSERT INTO FigureAreaParam 
+                    ( FigureId, ParameterName, ParameterValue ) VALUES
+                    ( @FigureId, @ParameterName, @ParameterValue )", param);
+            }
+
+            return id;
+        }
+    }
+}
