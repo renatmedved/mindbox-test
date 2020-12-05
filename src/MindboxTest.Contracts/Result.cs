0680cbae74b0aca37514483a716458fac69d2051
Renat <renatmusaev@yandex.ru>
2020-12-05 10:57:07 +0400

    result class

---
+++ src/MindboxTest.Contracts/Result.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System;
+using System.Linq;
+
+namespace MindboxTest.Contracts
+{
+    public sealed class Result<TData>
+    {
+        private readonly TData _data;
+        private readonly ReadOnlyCollection<string> _errs;
+
+        private Result(IEnumerable<string> errs, TData data)
+        {
+            _errs = errs?.ToList()?.AsReadOnly();
+            _data = data;
+        }
+
+        public static Result<TData> MakeSucces(TData data)
+        {
+            return new Result<TData>(null, data);
+        }
+
+        public static Result<TData> MakeFail(IEnumerable<string> errs)
+        {
+            if (errs == null)
+            {
+                throw new ArgumentNullException($"{nameof(errs)} should not be null");
+            }
+
+            return new Result<TData>(errs, default);
+        }
+
+        public ReadOnlyCollection<string> Errors
+        {
+            get
+            {
+                if (_errs == null)
+                {
+                    throw new InvalidOperationException($"property {nameof(Success)} should be false to read property {nameof(Errors)}");
+                }
+
+                return _errs;
+            }
+        }
+        public TData Data 
+        { 
+            get
+            {
+                if (_errs != null) 
+                {
+                    throw new InvalidOperationException($"property {nameof(Success)} should be true to read property {nameof(Data)}");
+                }
+
+                return _data;
+            }
+        }
+        public bool Success => _errs == null;
+        public bool Fail => !Success;
+    }
+}
