b1b7d27ae05fa5e8da22b71a4338ef437af525ef
Renat <renatmusaev@yandex.ru>
2020-12-06 16:19:09 +0400

    add empty

---
+++ src/MindboxTest.Contracts/Results/Empty.cs
@@ -0,0 +1,11 @@
+﻿namespace MindboxTest.Contracts.Results
+{
+    public class Empty
+    {
+        private static readonly Empty _instance = new Empty();//need only one instance
+
+        private Empty () { }
+
+        public static Empty Instance => _instance;
+    }
+}
