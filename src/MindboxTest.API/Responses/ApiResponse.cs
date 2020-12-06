70273c155d0a63137e0530d13f596e7241892330
Renat <renatmusaev@yandex.ru>
2020-12-06 16:22:22 +0400

    api response

---
+++ src/MindboxTest.API/Responses/ApiResponse.cs
@@ -0,0 +1,25 @@
+﻿using MindboxTest.Contracts.Results;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MindboxTest.API.Responses
+{
+    public class ApiResponse<TData>
+    {
+        public List<string> Errors { get; set; } = new List<string>();
+        public TData Data { get; set; }
+
+        public ApiResponse(Result<TData> result)
+        {
+            if (result.Fail)
+            {
+                Errors = result.Errors.ToList();
+            }
+            else
+            {
+                Data = result.Data; 
+            }
+        }
+    }
+}
