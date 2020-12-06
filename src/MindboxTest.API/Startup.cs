31c964e54cc5f1da67c843a9c0923907715fadef
Renat <renatmusaev@yandex.ru>
2020-12-06 16:23:09 +0400

    configure app

--- src/MindboxTest.API/Startup.cs
+++ src/MindboxTest.API/Startup.cs
@@ -1,15 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MediatR;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using MindboxTest.API.ModelBinders.Providers;
+using MindboxTest.DAL.Connections;
+using MindboxTest.DAL.QueryFactory;
+using MindboxTest.Figures.Proxy;
+using MindboxTest.Handlers.CalcArea;
 
 namespace MindboxTest.API
 {
@@ -25,7 +25,23 @@ namespace MindboxTest.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(options => 
+                options.ModelBinderProviders.Insert(0, new CustomBinderProvider()));
+            
+            ProxyFigureInitializator.Initialize();
+
+            services.AddSingleton<ProxyFigureStorage>()
+                .AddSingleton<ProxyFigureValidator>()
+                .AddSingleton<ProxyFigureCalculator>()
+                .AddSingleton<ProxyFigureDescriptionProvider>();
+
+            services.AddSingleton<IQueryFactory, QueryFactory>();
+
+            string conStr = Configuration.GetConnectionString("figures");
+
+            services.AddMediatR(typeof(CalcAreaHandler));
+
+            services.AddSingleton(_ => new ConnectionFactory(conStr));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
