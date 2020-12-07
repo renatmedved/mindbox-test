515d2398ab35099427ea3a9fcb7b9c3b7286ead6
Renat <renatmusaev@yandex.ru>
2020-12-07 20:01:17 +0400

    prepare to test

--- src/MindboxTest.Figures/AssemblyInfo.cs
+++ src/MindboxTest.Figures/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("MindboxTest.Figures.Tests")]
+[assembly: InternalsVisibleTo("MindboxTest.Handlers.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
