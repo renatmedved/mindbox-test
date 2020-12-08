662becfc623a4e90504ef7e900f094e88c303dd2
Renat <renatmusaev@yandex.ru>
2020-12-08 10:29:34 +0400

    fix deserialization

--- src/MindboxTest.API/Responses/ApiResponse.cs
+++ src/MindboxTest.API/Responses/ApiResponse.cs
@@ -10,6 +10,11 @@ namespace MindboxTest.API.Responses
         public List<string> Errors { get; set; } = new List<string>();
         public TData Data { get; set; }
 
+        public ApiResponse()
+        {
+
+        }
+
         public ApiResponse(Result<TData> result)
         {
             if (result.Fail)
